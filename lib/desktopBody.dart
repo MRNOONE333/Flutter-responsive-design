e2f33f39e0e2600044c19f18b77d98ba7f78de0f
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-15 16:46:35 +0530

    desktop file updated

---
+++ lib/desktopBody.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MydesktopBody extends StatelessWidget {
+  const MydesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
