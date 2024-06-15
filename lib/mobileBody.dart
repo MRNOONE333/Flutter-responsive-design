9a253c8bb715e9352f109dd32a45918739cd5407
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-15 16:48:37 +0530

    mobile screen started

---
+++ lib/mobileBody.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class MymobileBody extends StatelessWidget {
+  const MymobileBody({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
